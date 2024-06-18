9a6d0785bdc2cc9b22c2cf24742b4ca57c61ae03
Arthur Maciel <134110407+S0L4RT@users.noreply.github.com>
2024-06-17 22:22:22 -0300

    Update index.php

--- ProjetoNutri/view/index.php
+++ ProjetoNutri/view/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consultoria nutricional</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
     <header>
         <img src="https://cdn4.iconfinder.com/data/icons/wellness-tech-fill-wellbeing-performance/512/Meal_application_tracking-256.png" alt="logo">
@@ -13,18 +16,68 @@
     </header>
     <main>
         <div class="menu">
-            <li>Cadastro</li>
-            <li>Sobre</li>
-            <li>Contato</li>
+            <li><a href="cliente.php">Cadastro</a></li>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#contato">Contato</a></li>
         </div>
-        <div>
-            <h2>Sobre</h2>
-            <br><br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consectetur quidem quos perferendis dolorum illum, incidunt vitae nostrum cupiditate, atque eum perspiciatis impedit eaque placeat facilis, libero quasi autem non.</p>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="https://buzaneli.com.br/wp-content/uploads/2023/04/Empresa-_Nutric%CC%A7a%CC%83o01.jpg" class="d-block w-100" style="height: 600px;" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="https://images.ecycle.com.br/wp-content/uploads/2022/10/13150727/51138312872_19796786a8_o-scaled.jpg.webp" class="d-block w-100" alt="..." style="height: 600px;">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://webrun.com.br/wp-content/uploads/2019/08/AdobeStock_196358415.jpeg" class="d-block w-100" alt="..." style="height: 600px;">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
+
+        <section>
+            <div class="sobre">
+                <h2 id="sobre">Sobre</h2>
+                <br><br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consectetur quidem quos perferendis dolorum illum, incidunt vitae nostrum cupiditate, atque eum perspiciatis impedit eaque placeat facilis, libero quasi autem non. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora reprehenderit cum, expedita debitis deserunt illum veritatis eius natus iste minima, cupiditate soluta itaque vero earum consectetur quia totam fugit facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque laboriosam, aliquid officiis similique quasi nam voluptates, reprehenderit in ex maiores at enim. Animi suscipit voluptatem mollitia qui architecto a possimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatum laboriosam, voluptates totam quibusdam consequuntur, ut repellat possimus illum optio animi reiciendis eaque fugit voluptas cum adipisci, officiis tempore dolores!</p>
+            </div>
+        </section>
+
+        <section class="contato">
+            <div class="container-contato">
+                <div id="contato">
+                    <h2>Contato</h2>
+                    <br><br>
+                    <p>Telefone:</p>
+                    <p>(51) 99999-9999</p>
+                    <br>
+                    <p>E-mail:</p>
+                    <p>draadriana.melo@gmail.com</p>
+                    <br>
+                    <p>Instagram:</p>
+                    <p>@dra_adrianamelo</p>
+                </div>
+
+                <div class="mapa">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55377.91586061942!2d-51.19830739547716!3d-29.831965299999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95196fff42954a99%3A0xdc3b5f1337ad3c41!2sNutricionista%20Juliana%20Velleda%20de%20Souza!5e0!3m2!1spt-BR!2sbr!4v1718667183465!5m2!1spt-BR!2sbr" style="height: 325px; width: 600px;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </section>
+
     </main>
     <footer>
-
+        Copyright Arthur Maciel da Rosa
     </footer>
 </body>
+
 </html>
